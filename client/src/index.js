b7521eeb94ff8c548b2f4e820034b9273c0d6549
ohanthony <ohanthony95@gmail.com>
2023-02-08 01:05:44 +0900

    fist commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import * as redux from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = redux.applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(redux.createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
