309dd0c04b65e45d4ebb4d4b3d775d34f9677673
ohanthony <ohanthony95@gmail.com>
2023-02-09 17:19:55 +0900

    default setting

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <a href="/">Togather</a>
+      <div>
+        <button>글쓰기</button>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
