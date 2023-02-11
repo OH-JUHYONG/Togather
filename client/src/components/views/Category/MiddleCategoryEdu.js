f973d064023ee28b51377bbf4207b5a45c06513d
ohanthony <ohanthony95@gmail.com>
2023-02-12 01:30:31 +0900

    middle category

---
+++ client/src/components/views/Category/MiddleCategoryEdu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MiddleCategory.css';
+
+function MiddleCategoryEdu() {
+  return (
+    <div className="middle-category">
+      <div className="middle-category__name">
+        <span>전체</span>
+      </div>
+      <div className="middle-category__name">
+        <span>학교 수업</span>
+      </div>
+      <div className="middle-category__name">
+        <span>대회&공모전</span>
+      </div>
+      <div className="middle-category__name">
+        <span>스터디</span>
+      </div>
+    </div>
+  );
+}
+
+export default MiddleCategoryEdu;
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LandingPage.css';
 
+import MiddleCategoryEdu from '../Category/MiddleCategoryEdu';
+
 function LandingPage() {
+  const [visibleEdu, setVisibleEdu] = useState(false);
+
+  const alertPage = () => {
+    alert('서비스 준비 중입니다.');
+  };
   return (
-    <div className="high-category">
-      <div className="high-category__name">
-        <span>교육</span>
+    <>
+      <div className="high-category">
+        <div className="high-category__name">
+          <button
+            className="high-category__button"
+            onClick={() => {
+              setVisibleEdu(!visibleEdu);
+            }}
+          >
+            교육
+          </button>
+        </div>
+        <div className="high-category__name">
+          <span onClick={alertPage}>취미</span>
+        </div>
+        <div className="high-category__name">
+          <span onClick={alertPage}>동아리</span>
+        </div>
       </div>
-      <div className="high-category__name">
-        <span>취미</span>
-      </div>
-      <div className="high-category__name">
-        <span>동아리</span>
-      </div>
-    </div>
+
+      {visibleEdu && <MiddleCategoryEdu />}
+    </>
   );
 }
 
