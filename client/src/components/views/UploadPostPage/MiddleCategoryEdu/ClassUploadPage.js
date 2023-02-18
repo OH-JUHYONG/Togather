91864e60d20caa1d82c096d6eedf87252a9d198c
ohanthony <ohanthony95@gmail.com>
2023-02-18 15:13:36 +0900

    before desing

--- client/src/components/views/UploadPostPage/MiddleCategoryEdu/ClassUploadPage.js
+++ client/src/components/views/UploadPostPage/MiddleCategoryEdu/ClassUploadPage.js
@@ -1,5 +1,4 @@
 // import React, { useState } from 'react';
-// import './ClassUploadPage.css';
 // import { Button, Form, Input } from 'antd';
 // import DatePicker from 'react-datepicker'; // 달려을 가져오기 위한 명령어
 // import Axios from 'axios';
@@ -12,36 +11,36 @@
 
 // // 모집 인원 option
 // const HeadCountArray = [
-//   { key: 0, value: '인원 미정' },
-//   { key: 1, value: '2명' },
-//   { key: 2, value: '3명' },
-//   { key: 3, value: '4명' },
-//   { key: 4, value: '5명' },
-//   { key: 5, value: '6명' },
-//   { key: 6, value: '7명' },
-//   { key: 7, value: '8명' },
-//   { key: 8, value: '9명' },
-//   { key: 9, value: '10명' },
+//   { key: 1, value: '인원 미정' },
+//   { key: 2, value: '2명' },
+//   { key: 3, value: '3명' },
+//   { key: 4, value: '4명' },
+//   { key: 5, value: '5명' },
+//   { key: 6, value: '6명' },
+//   { key: 7, value: '7명' },
+//   { key: 8, value: '8명' },
+//   { key: 9, value: '9명' },
+//   { key: 10, value: '10명' },
 // ];
 
 // // 진행 방식 option
 // const ProgessArray = [
-//   { key: 0, value: '대면' },
-//   { key: 1, value: '비대면' },
-//   { key: 2, value: '혼용' },
-//   { key: 3, value: '추후 결정' },
+//   { key: 1, value: '대면' },
+//   { key: 2, value: '비대면' },
+//   { key: 3, value: '혼용' },
+//   { key: 4, value: '추후 결정' },
 // ];
 
 // // 연락 방법 option
 // const ContactArray = [
 //   {
-//     key: 0,
+//     key: 1,
 //     value: '카카오톡 오픈채팅',
 //     placeholder: '오픈 채팅 링크를 입력해주세요.',
 //   },
-//   { key: 1, value: '디스코드', placeholder: '디스코드 링크를 입력해주세요.' },
-//   { key: 2, value: '이메일', placeholder: '이메일 주소를 입력해주세요.' },
-//   { key: 3, value: '전화번호', placeholder: '전화번호를 입력해주새요.' },
+//   { key: 2, value: '디스코드', placeholder: '디스코드 링크를 입력해주세요.' },
+//   { key: 3, value: '이메일', placeholder: '이메일 주소를 입력해주세요.' },
+//   { key: 4, value: '전화번호', placeholder: '전화번호를 입력해주새요.' },
 // ];
 
 // const ClassUploadPage = (props) => {
@@ -49,10 +48,10 @@
 
 //   const [Division, setDivision] = useState(''); // 수업명 / 분반
 //   const [Title, setTitle] = useState(''); // 제목
-//   const [HeadCount, setHeadCount] = useState(0); // 모집 인원
+//   const [HeadCount, setHeadCount] = useState(1); // 모집 인원
 //   const [SelectedDate, setSelectedDate] = useState(null); //모집 기한
-//   const [Progress, setProgress] = useState(0); // 진행 방식
-//   const [Contact, setContact] = useState(0); // 연락 방법
+//   const [Progress, setProgress] = useState(1); // 진행 방식
+//   const [Contact, setContact] = useState(1); // 연락 방법
 //   const [Contactinfo, setContactinfo] = useState(''); // 연락 정보
 
 //   const [Description, setDescription] = useState(''); // 상세 설명
@@ -119,10 +118,10 @@
 //       m_category: '학교 수업',
 //       divison: Division,
 //       title: Title,
-//       headcount: HeadCountArray[HeadCount].value,
+//       headcount: HeadCountArray[HeadCount - 1].value,
 //       day: SelectedDate,
-//       progress: ProgessArray[Progress].value,
-//       contact: ContactArray[Contact].value,
+//       progress: ProgessArray[Progress - 1].value,
+//       contact: ContactArray[Contact - 1].value,
 //       contactinfo: Contactinfo,
 //       description: Description,
 //     };
@@ -139,90 +138,89 @@
 
 //   return (
 //     <>
-//       <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-//         <Form onSubmit={submitHandler}>
-//           <br />
-//           <label>수업명 / 분반</label>
-//           <br />
-//           <Input
-//             onChange={divisionChangeHandler}
-//             value={Division}
-//             placeholder="ex)... 게임혼합현실 / 7분반"
-//           />
-//           <br />
+//       <Form onSubmit={submitHandler}>
+//         <br />
+//         <br />
+//         <label>수업명 / 분반</label>
+//         <br />
+//         <Input
+//           onChange={divisionChangeHandler}
+//           value={Division}
+//           placeholder="ex)... 게임혼합현실 / 7분반"
+//         />
+//         <br />
 
-//           <br />
-//           <label>제목</label>
-//           <br />
-//           <Input
-//             onChange={titleChangeHandler}
-//             value={Title}
-//             placeholder="제목을 입력해 주세요"
-//           />
-//           <br />
+//         <br />
+//         <label>제목</label>
+//         <br />
+//         <Input
+//           onChange={titleChangeHandler}
+//           value={Title}
+//           placeholder="제목을 입력해 주세요"
+//         />
+//         <br />
 
-//           <br />
-//           <label>모집인원</label>
-//           <select onChange={headcounterChangeHandler} value={HeadCount}>
-//             {HeadCountArray.map((item) => (
-//               <option key={item.key} value={item.key}>
-//                 {item.value}
-//               </option>
-//             ))}
-//           </select>
-//           <br />
+//         <br />
+//         <label>모집인원</label>
+//         <select onChange={headcounterChangeHandler} value={HeadCount}>
+//           {HeadCountArray.map((item) => (
+//             <option key={item.key} value={item.key}>
+//               {item.value}
+//             </option>
+//           ))}
+//         </select>
+//         <br />
 
-//           <br />
-//           <label>모집기한</label>
-//           <DatePicker
-//             selected={SelectedDate}
-//             onChange={selectedDateChangeHandler}
-//           />
-//           <br />
+//         <br />
+//         <label>모집기한</label>
+//         <DatePicker
+//           selected={SelectedDate}
+//           onChange={selectedDateChangeHandler}
+//         />
+//         <br />
 
-//           <br />
-//           <label>진행 방식</label>
-//           <select onChange={progressChangeHandler} value={Progress}>
-//             {ProgessArray.map((item) => (
-//               <option key={item.key} value={item.key}>
-//                 {item.value}
-//               </option>
-//             ))}
-//           </select>
-//           <br />
+//         <br />
+//         <label>진행 방식</label>
+//         <select onChange={progressChangeHandler} value={Progress}>
+//           {ProgessArray.map((item) => (
+//             <option key={item.key} value={item.key}>
+//               {item.value}
+//             </option>
+//           ))}
+//         </select>
+//         <br />
 
-//           <br />
-//           <label>연락 방법</label>
-//           <select onChange={contactChangeHandler} value={Contact}>
-//             {ContactArray.map((item) => (
-//               <option key={item.key} value={item.key}>
-//                 {item.value}
-//               </option>
-//             ))}
-//           </select>
-//           <br />
-//           <Input
-//             onChange={contactinfoChangeHandler}
-//             value={Contactinfo}
-//             placeholder={ContactArray[Contact].placeholder}
-//           />
-//           <br />
+//         <br />
+//         <label>연락 방법</label>
+//         <select onChange={contactChangeHandler} value={Contact}>
+//           {ContactArray.map((item) => (
+//             <option key={item.key} value={item.key}>
+//               {item.value}
+//             </option>
+//           ))}
+//         </select>
+//         <br />
+//         <Input
+//           onChange={contactinfoChangeHandler}
+//           value={Contactinfo}
+//           placeholder={ContactArray[Contact - 1].placeholder}
+//         />
+//         <br />
 
-//           <br />
-//           <label>상세 설명</label>
+//         <br />
+//         <label>상세 설명</label>
 
-//           <br />
-//           <TextArea
-//             style={{ height: '100vh' }}
-//             onChange={descriptionChangeHandler}
-//             value={Description}
-//           />
-//           <br />
+//         <br />
+//         <TextArea
+//           style={{ height: '100vh' }}
+//           onChange={descriptionChangeHandler}
+//           value={Description}
+//         />
+//         <br />
 
-//           <br />
-//           <Button onClick={submitHandler}>글등록</Button>
-//         </Form>
-//       </div>
+//         <br />
+//         <Button onClick={submitHandler}>글등록</Button>
+//       </Form>
 //     </>
 //   );
 // };
